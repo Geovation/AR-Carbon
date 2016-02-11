45051fcc05674b53b2ec5d7a77de48f5f296dbe1
JMilner <jmilner@nd29136.local>
2016-02-11 14:39:57 +0000

    Escape geojson, user removed edit/delete after submit

--- arcarbon-map.php
+++ arcarbon-map.php
@@ -6,7 +6,7 @@
  * Plugin Name:       AR Carbon Map
  * Plugin URI:        http://www.geovation.uk
  * Description:       The map element of the AR Carbon Site
- * Version:           1.0.9
+ * Version:           1.0.10
  * Author:            James Milner
  * Author URI:        http://www.geovation.uk
  * License:           GPL-2.0+
@@ -91,8 +91,8 @@ function run_arcarbon_map() {
 			?>
 
 			<script type="text/javascript">
-				var MAPBOX_API_KEY = '<?php echo get_option( "map_api_key"); ?>'
-   				var USER_GEOJSON = '<?php echo get_user_meta( get_current_user_id(), "arcarbon_map_geojson", true); ?>'
+				var MAPBOX_API_KEY = "<?php echo get_option( "map_api_key"); ?>";
+   				var USER_GEOJSON = "<?php echo addslashes(get_user_meta( get_current_user_id(), "arcarbon_map_geojson", true)); ?>";
 			</script>
 
 			<link rel="stylesheet" type='text/css' href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
