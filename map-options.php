cdee2d3ebc36c231a22f39b3e61c6c7fe9eca656
JMilner <jmilner@nd29136.local>
2016-03-10 11:34:44 +0000

    Added options and customised materialize CSS

--- map-options.php
+++ map-options.php
@@ -13,10 +13,32 @@ function ar_carbon_map_create_menu() {
 
 function register_ar_carbon_map_settings() {
     //register our settings
-    register_setting( 'ar-carbon-map-settings-group', 'map_api_key' );
+    register_setting( 'ar-carbon-map-settings-group', 'arcarbon_map_api_key' );
+    register_setting( 'ar-carbon-map-settings-group', 'arcarbon_map_page_to_use' );
 }
 
-function ar_carbon_map_settings_page() { ?>
+function ar_carbon_map_settings_page() {
+    $args = array(
+        'sort_order' => 'asc',
+        'sort_column' => 'post_title',
+        'hierarchical' => 1,
+        'exclude' => '',
+        'include' => '',
+        'meta_key' => '',
+        'meta_value' => '',
+        'authors' => '',
+        'child_of' => 0,
+        'parent' => -1,
+        'exclude_tree' => '',
+        'number' => '',
+        'offset' => 0,
+        'post_type' => 'page',
+        'post_status' => 'publish'
+    );
+    $pages = get_pages();
+
+
+    ?>
 
     <div class="wrap">
     <h2>AR Carbon Map</h2>
@@ -27,8 +49,24 @@ function ar_carbon_map_settings_page() { ?>
         <table class="form-table">
             <tr valign="top">
             <th scope="row">Map API Key</th>
-            <td><input type="text" name="map_api_key" value="<?php echo esc_attr( get_option('map_api_key') ); ?>" /></td>
+            <td><input type="text" name="arcarbon_map_api_key" value="<?php echo esc_attr( get_option('map_api_key') ); ?>" /></td>
             </tr>
+            <tr>
+            <th scope="row">Page to Use</th>
+            <td>
+                <select type="text" name="arcarbon_map_page_to_use"/>
+                <option>Choose a Page</option>
+                <?php
+                    foreach($pages as $page) {
+                        $page_title = $page->post_title;
+                        $page_ID    = $page->ID;
+                        echo "<option value='$page_ID'>$page_title</option>";
+                    }
+                ?>
+                </select>
+            </td>
+            <tr>
+
         </table>
 
         <?php submit_button(); ?>
